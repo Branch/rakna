4ed71b4ed4e22ebe5b9a427a8a141e7b6e96ea53
Andreas Beuger <andreasbeuger@gmail.com>
2022-09-28 16:14:12 +0200

    Updated nav menu

--- components/layouts/sidebar/SidebarLayout.tsx
+++ components/layouts/sidebar/SidebarLayout.tsx
@@ -1,19 +1,33 @@
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 export interface ISidebarLayout {}
 
 const SidebarLayout = () => {
+    const [isScrolledToTop, toggleScrollToTop] = useState(true)
+
+    // Used to toggle menu color
+    useEffect(() => {
+        window.onscroll = () => {
+            if (window.pageYOffset !== 0) {
+                toggleScrollToTop(false)
+            } else {
+                toggleScrollToTop(true)
+            }
+        }
+    }, [])
+
     return (
-        <nav>
-            <input placeholder="Search..." />
+        <nav
+            className={`transition ease-linear delay-50 sticky z-10 flex p-8 gap-5 top-0 mx-auto ${
+                isScrolledToTop ? 'bg-smoothYellow-50' : 'bg-white shadow-sm'
+            }`}
+        >
             <Link href="/">
                 <a>Home</a>
             </Link>
             <Link href="/about">
                 <a>About</a>
-            </Link>
-            <Link href="/contact">
-                <a>Contact</a>
             </Link>
         </nav>
     )
