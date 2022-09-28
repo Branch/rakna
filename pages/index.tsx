bec6d0301e97bd6b3644f5cd9153fa6fec8b5c51
Andreas Beuger <andreasbeuger@gmail.com>
2022-09-28 16:13:27 +0200

    Updated index, WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import svg from '../assets/images/top-pic.svg'
+
 import CatCard from '../components/cards/cat/CatCard'
 import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks'
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout'
@@ -9,22 +11,39 @@ import { NextPageWithLayout } from './page'
 
 const Home: NextPageWithLayout = () => {
     return (
-        <div className="flex justify-center flex-col items-center min-h-screen">
+        <div className="">
             <Head>
-                <title>Create Next App</title>
+                <title>Räkna på bolån</title>
                 <meta name="description" content="Räkna på bolån" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
-            <main>
-                <h1>Räkna på bolån!</h1>
-                <div>
-                    <CatCard {...mockCatCardProps.base} />
-                    <CatCard {...mockCatCardProps.base} />
-                    <CatCard {...mockCatCardProps.base} />
-                    <CatCard {...mockCatCardProps.base} />
+            <div className="bg-smoothYellow-50 py-20">
+                <div className="flex justify-center items-center">
+                    <div className="flex flex-row gap-36">
+                        <div className="flex flex-col justify-center gap-4">
+                            <h1 className="font-Playfair text-5xl">
+                                Räkna på bolån!
+                            </h1>
+                            <p className="max-w-md">
+                                Använd kalkylatorn nedan för att se vad just du
+                                skulle betala för olika räntor
+                            </p>
+                        </div>
+                        <Image
+                            alt="Graph hero image"
+                            src={svg}
+                            layout="intrinsic"
+                        />
+                    </div>
                 </div>
-            </main>
+            </div>
+
+            <div>
+                <CatCard {...mockCatCardProps.base} />
+                <CatCard {...mockCatCardProps.base} />
+                <CatCard {...mockCatCardProps.base} />
+                <CatCard {...mockCatCardProps.base} />
+            </div>
         </div>
     )
 }
